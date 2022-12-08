6e819032edb7970ddf3d7ea1bab76d7bee9e922d
Henry Escobar <henrye@gmail.com>
2022-12-07 17:06:51 -0800

    Table Migrations

---
+++ db/migrate/20221208010005_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trips do |t|
+      t.string :destination
+      t.integer :cost
+      t.date :check_in
+      t.date :check_out
+      t.integer :user_id
+      t.integer :hotel_id
+    end
+  end
+end
