1f5ac3375f1f847aae0dacedeb6e0309966e409c
Henry Escobar <henrye@gmail.com>
2022-12-14 21:38:42 -0800

    Added username column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_010030) do
+ActiveRecord::Schema.define(version: 2022_12_15_053255) do
 
   create_table "hotels", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_12_08_010030) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
